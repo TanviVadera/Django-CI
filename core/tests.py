b11d52c36e144dd594a1180f91b31fb17aa35c26
TanviVadera <tanvivadera12@gmail.com>
2026-02-02 22:49:49 +0530

    Intial commit

---
+++ core/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+class SimpleTest(TestCase):
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
